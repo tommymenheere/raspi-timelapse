dd52c99b892b201a9f71a3e7ac96edbaf148d1fc
Tommy Menheere <tommym@tommym.net>
2017-01-05 17:28:40 +0100

    copied files using scp

--- bin/capture.sh
+++ bin/capture.sh
@@ -1,12 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-pgrep raspistill > /dev/null && echo `date +”%Y-%m-%d %H:%M:%S"` raspistill running && exit 0
+pgrep raspistill > /dev/null && echo `date +"%Y-%m-%d %H:%M:%S"` raspistill running && exit 0
+
+if [ $# -ne 2 ]; then
+	echo "usage $0 <path> <next>"
+	exit 1
+fi
 
 DIR=$1
 NEXT=$2
-TMP_FILE=$(printf "tmp_%05d.jpg" $NEXT )
 FILE=$(printf "%05d.jpg" $NEXT )
 DATE=`date +"%Y-%m-%dT%H:%M:%S%z"`
-raspistill -w 960 -h 720 -n -o $DIR/$TMP_FILE && echo `date +”%Y-%m-%d %H:%M:%S"` captured to $DIR/$TMP_FILE && convert -font courier-bold -fill yellow -pointsize 36 -draw "text 15,710 'NIJMEGEN $DATE'" $DIR/$TMP_FILE $DIR/$FILE && echo `date +”%Y-%m-%d %H:%M:%S"` wrote file $DIR/$FILE && rm $DIR/$TMP_FILE
+raspistill -w 960 -h 720 -n -o - | convert - -font courier-bold -fill yellow -pointsize 36 -draw "text 15,710 'NIJMEGEN $DATE'" fd:1 |  ssh $USER@$HOST "cat - > $DIR/$FILE" 
+echo "wrote $USER@$HOST:$DIR/$FILE" 
 RETURN=$?
 exit $RETURN
--- bin/timelapse.sh
+++ bin/timelapse.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 
-ROOT=/vol/data/timelapse
+ROOT=/Volumes/Data2/timelapse/
 DIR=$ROOT/$(date +"%Y/%m/%d")
 INTERVAL=10
 NEXT=0
+HOST=strangelove.local
+USER=tommym
 
-echo `date +"%y%m%m %H:%M:%S"` Started 
+export HOST
+export USER
+export PATH=/usr/bin:$PATH
 
-if (cd $DIR ; ls *.jpg ) > /dev/null 2>&1 ;
+PRG_DIR=$(dirname $0)
+ 
+echo `date +"%Y%m%d %H:%M:%S"` Started 
+
+if ssh $USER@$HOST ls $DIR/*.jpg > /dev/null 2>&1 ;
 then
-        LAST=$(cd $DIR ; ls -r ?????.jpg | head -1 | sed 's/.jpg//g')
+        LAST=$(ssh $USER@$HOST "cd $DIR ; ls -r ?????.jpg" | head -1 | sed 's/.jpg//g')
         NEXT=$((10#$LAST + 1 ))
 else
         NEXT=0
+fi
+
+if [ ! -d log ]; then
+	mkdir log
 fi
 
 # Create screenshot
 while true ;
 do
 	DIR=$ROOT/$(date +"%Y/%m/%d")
-	if [ ! -d $DIR ]; then
-  		mkdir -p $DIR
+	
+	if ! ssh $USER@$HOST ls $DIR; then
+  		ssh $USER@$HOST mkdir -p "$DIR"
 		NEXT=0
-		echo `date +"%y%m%m %H:%M:%S"` created dir $DIR
+		echo `date +"%y%m%m %H:%M:%S"` created dir $USER@$HOST:$DIR  
 	fi
-	/home/pi/bin/capture.sh $DIR $NEXT >> ~/log/timelapse.log 2>&1 &
+	/bin/bash ${PRG_DIR}/capture.sh $DIR $NEXT 2>&1 &
 	NEXT=$(( 10#$NEXT + 1 ))
 	sleep $INTERVAL
 done
